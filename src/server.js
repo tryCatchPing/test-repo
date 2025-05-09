96819969858fbdfd8c23c7ef765676409997a29c
ehdnd_mac <taeungjang@naver.com>
2025-05-09 12:41:43 +0900

    feat: fabric 테스트 가능 상태 확보

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 import express from "express";
+import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 
 const PORT = 3000;
 
 const app = express();
-
-app.use(express.json());
+const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 
 app.use("/", rootRouter);
+
+app.use("/static", express.static("src/public"));
 
 app.listen(PORT, (req, res) => {
   console.log(`⚡️ Server is running on http://localhost:${PORT}`);
